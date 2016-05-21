5d546a25d2137b32651c3fb73271ab66d239efca
zhang-edward <edwardz123@gmail.com>
2016-05-21 14:30:22 -0700

    Add Unity Project

---
+++ Assets/GameManager.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+	private float[] rhythm = new float[] {8, 8, 8, 3, 1, 2, 2};
+	private int rhythmIndex = 0;
+	private float time;
+
+	public AudioClip[] slamSounds;
+	private AudioSource audioSource;
+
+	public int combo = 0;
+	public Shake locker;
+	public Shake cameraShake;
+
+	public Text debugText;
+
+	private SpriteRenderer sr;
+
+	// Use this for initialization
+	void Start () {
+		sr = this.GetComponent<SpriteRenderer> ();
+		audioSource = this.GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		// DEBUG
+		debugText.text = "" + (time - rhythm [rhythmIndex]);
+		// DEBUG
+
+		time += Time.deltaTime * 8;
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			// Player hit key on rhythm
+			if (Mathf.Abs(time - rhythm[rhythmIndex]) <= 1.0f || rhythmIndex == 0)
+			{
+				Beat ();
+				rhythmIndex++;
+				Debug.Log (rhythmIndex);
+				if (rhythmIndex > rhythm.Length - 1)
+				{
+					combo++;
+					rhythmIndex = 0;
+				}
+			}
+			// player did not hit the key on rhythm
+			else
+			{
+				// Reset all variables
+				rhythmIndex = 0;
+				combo = 0;
+			}
+			time = 0;
+		}
+		/*if (time >= rhythm[rhythmIndex])
+		{
+			Beat ();
+			time = 0;
+			rhythmIndex++;
+			if (rhythmIndex > rhythm.Length - 1)
+			{
+				rhythmIndex = 0;
+			}
+		}*/
+	}
+
+	private void Beat()
+	{
+		locker.Activate (0.25f, 0.05f);
+		cameraShake.Activate (0.1f, 0.10f);
+		RandomizeSFX (0.5f);
+
+		StartCoroutine ("BeatCR");
+	}
+
+	private IEnumerator BeatCR()
+	{
+		sr.color = Color.black;
+		yield return new WaitForSeconds (0.05f);
+		sr.color = Color.white;
+	}
+
+	private void RandomizeSFX(float pitchOffset)
+	{
+		audioSource.clip = slamSounds [Random.Range (0, slamSounds.Length)];
+		audioSource.pitch = Random.Range (-pitchOffset, pitchOffset) + 1.0f;
+		audioSource.Play ();
+	}
+}
---
+++ Assets/Shake.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shake : MonoBehaviour {
+
+	private Vector3 origin;
+	private Transform tr;
+
+	private bool shaking;
+	private float cooldown;
+	private float magnitude;
+
+	void Awake()
+	{
+		tr = transform;
+		origin = transform.position;
+	}
+
+	public void Activate(float magnitude, float time)
+	{
+		shaking = true;
+		cooldown = time;
+		this.magnitude = magnitude;
+	}
+
+	void Update()
+	{
+		cooldown -= Time.deltaTime;
+		if (cooldown <= 0)
+			shaking = false;
+		
+		if (shaking)
+		{
+			float z = origin.z;
+			float randX = Random.Range (-magnitude, magnitude);
+			float randY = Random.Range (-magnitude, magnitude);
+			tr.position = new Vector3 (randX, randY, z);
+			magnitude -= 0.01f;
+		}
+		else
+		{
+			tr.position = origin;
+		}
+	}
+}
